2e39e1f841ea01a2359deddcc4efc9f5fcadc880
Josh Kim <realx1212@gmail.com>
2024-11-08 02:02:51 +0900

    Update josh_quest 4.py

--- josh_quest 4.py
+++ josh_quest 4.py
@@ -1,5 +1,5 @@
 
-'''정수 계산기를 실행하는 프로그램 모듈입니다.'''
+''' This is a program module that runs an integer calculator.'''
 
 import math
 
@@ -10,27 +10,28 @@ def integer_input(prompt):
             input_num = int(input(prompt))
             return input_num
         except ValueError: #2 잘못된 입력 → 예외처리
-            print("잘못된 입력입니다. 정수를 입력해주세요.") 
+            print("Invalid input. Please enter an integer.") 
 
 
 
 # 연산식 정의
 def calculator():
-    num1 = integer_input("첫번째 정수를 입력하세요: ")
+    
+    num1 = integer_input("Input first number: ")
 
-
-    '''연산자 입력에 관한 예외 처리 과정 = while, break, else로 구성'''
+    ''' Exception handling process for operator input = while, break, else'''
+    
     while True:
-        operator = input("연산자를 입력하세요(+, -, *, /, **): ")
+        operator = input("Input operator(+, -, *, /, **): ").strip() ### .strip() 추가 중요. 공백포함시 연산자 인식오류
         if operator in ['+','-','*','/','**']:
             break
         else:
-            print("지원하지 않는 연산자입니다.") #4 지원하지 않는 연산자 예외처리
+            print("Unsupported operator.") #4 지원하지 않는 연산자 예외처리
             
-    num2 = integer_input("두번째 정수를 입력하세요: ")
+    num2 = integer_input("Input second number: ")
 
 
-    '''계산 결과 반환 과정 = try, if, elif, else로 구성'''
+    ''' Calculation result return process = try, if, elif, else'''
     try:
         if operator == '+':
             result = num1 + num2
@@ -43,11 +44,11 @@ def calculator():
         elif operator == '**':
             result = math.pow(num1, num2) #5 math.pow 함수모듈 사용
         
-        print(f"계산결과: {num1} {operator} {num2} = {result}")
+        print(f"Result: {num1} {operator} {num2} = {result}")
         return result
 
     except ZeroDivisionError:
-        print("0으로 나눌 수 없습니다.") #3 나눗셈에서 0으로 나눌 수 없는 경우 예외처리
+        print("Cannot be divided by zero.") #3 나눗셈에서 0으로 나눌 수 없는 경우 예외처리
         return None
         
 
@@ -55,18 +56,18 @@ def calculator():
 
 
 # 계산 실행 
-
 while True:
-    result = calculator()
-    continue_input = input("계속하시겠습니까? (y/n): ")
+    calculator()
+    continue_input = input("Continue? (y/n): ").strip().lower()  
+    #6 올바른 입력이 들어올 때까지 반복
+    while continue_input not in ['y', 'n']:
+        print("Invalid input. Please enter y or n.")
+        continue_input = input("Continue? (y/n): ").strip().lower()  
     
-    if continue_input not in ['y','n']:
-        print("잘못된 입력입니다. y 또는 n을 입력해주세요.")
-        continue_input = input("계속하시겠습니까? (y/n): ").lower()
-    
-    elif continue_input == 'n':
-        print("계산기를 종료합니다.")
+    if continue_input == 'n':
+        print("Calculator is terminated.")
         break
+
 
 
 '''
@@ -83,3 +84,4 @@ $$$김대환(코더) 회고$$$
 3. 구조 수정 과정에서 제로디비젼에 대한 return값을 none으로 설정하면 해결되었는데 이부분에 대한 follow-up 과정이 필요했다.
 4. Quest 03까지 진행해오면서 성장한 자신을 발견하고 뿌듯함을 느꼈다.   
 '''
+
