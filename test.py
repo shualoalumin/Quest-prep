a9b91d5a56c543244b29ceb7cf2df3e92d6ef830
Josh Kim <realx1212@gmail.com>
2024-11-10 03:00:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,24 @@
-# 연산자함수 테스트
+# 부모 클래스
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    
+    def make_sound(self):
+        print("동물이 소리를 냅니다")
 
-import math
+# 자식 클래스        
+class Dog(Animal):  # Animal 클래스를 상속
+    def __init__(self, name, breed):
+        super().__init__(name)  # 부모 클래스의 __init__ 호출
+        self.breed = breed
+    
+    def make_sound(self):  # 메서드 오버라이딩
+        print("멍멍!")
+        
+    def fetch(self):  # 새로운 메서드 추가
+        print(f"{self.name}이(가) 공을 가져옵니다")
 
-def operator_input():
-        operator = input("연산자를 입력하세요(+,-,*,/,**): ")
-        if operator in ['+','-','*','/','**']:
-            return operator
-
-        else:
-            print("지원하지 않는 연산자입니다.")
+# 사용 예시
+my_dog = Dog("바둑이", "진돗개")
+my_dog.make_sound()  # 출력: 멍멍!
+my_dog.fetch()  # 출력: 바둑이이(가) 공을 가져옵니다
