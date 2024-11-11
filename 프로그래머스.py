f6595a405ca2e7b9a82a897cc9fb259ea81fc1a5
Josh Kim <realx1212@gmail.com>
2024-11-12 03:57:52 +0900

    Update 프로그래머스.py

--- 프로그래머스.py
+++ 프로그래머스.py
@@ -1,1 +1,12 @@
-asdf
+while True:
+    try:
+        str = input("문자열을 입력하세요: ").replace(" ", "")
+        
+        if not 1 <= len(str) <= 1000000:
+            raise ValueError("길이가 1~1,000,000 범위를 벗어났습니다.")
+        
+        print(f"결과: {str}")
+        break
+        
+    except ValueError as e:
+        print(f"오류: {e}")
