59029771a2e9b79c5a28c0d3fe28939921ec1b57
Josh Kim <realx1212@gmail.com>
2024-11-13 02:23:38 +0900

    Update 데이타분석1_numpy.py

--- 데이타분석1_numpy.py
+++ 데이타분석1_numpy.py
@@ -1,22 +1,35 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-# 화살표 스타일 리스트
-styles = ['-', '->', '-[', '|-|', '-|>', '<-', '<->', 'fancy', 'simple', 'wedge']
+# Figure와 Axes 생성 (2행 1열)
+fig, axes = plt.subplots(2, 1, figsize=(8, 10))
 
-# 그래프 설정
-fig, ax = plt.subplots(figsize=(12, 8))
-ax.set_xlim(-1, 4)
-ax.set_ylim(-len(styles), 1)
+# 샘플 데이터 생성
+data = pd.Series(np.random.rand(5), index=list('abcde'))
 
-# 각 스타일별 화살표 그리기
-for i, style in enumerate(styles):
-    ax.annotate('', xy=(3, -i), xytext=(0, -i),
-                arrowprops=dict(arrowstyle=style, color='red'))
-    ax.text(-0.5, -i, style, ha='right', va='center')
+# 수직 막대 그래프
+data.plot(
+    kind='bar', 
+    ax=axes[0],
+    color='blue',
+    alpha=0.7,
+    title='Vertical Bar Plot',
+    grid=True
+)
+axes[0].set_ylabel('Values')
 
-plt.title('Matplotlib Arrow Styles')
-ax.grid(False)
-ax.set_axis_off()
+# 수평 막대 그래프
+data.plot(
+    kind='barh',
+    ax=axes[1],
+    color='red',
+    alpha=0.7,
+    title='Horizontal Bar Plot',
+    grid=True
+)
+axes[1].set_xlabel('Values')
+
+# 레이아웃 조정
 plt.tight_layout()
 plt.show()
